cacae65b5603b42f9c697f611d6f411829f0e45d
Aashutosh Bansal <75746087+coderAashutosh@users.noreply.github.com>
2023-08-02 19:37:48 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Aashutosh',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://nextjs-notion-starter-p5qgjv9nm-coderaashutosh.vercel.app/',
   author: 'Aashutosh Bansal',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Blogs',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'AashutoshBansa7',
+  github: 'coderAashutosh',
+  linkedin: 'aashutosh-bansal',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
